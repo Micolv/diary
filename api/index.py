3f86440206011cf7e0e7cd68d7814775814a2dba
thund1r <thuncoder@foxmail.com>
2022-05-12 00:00:22 +0800

    TEST

--- api/index.py
+++ api/index.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
 
-app = FastAPI(
-    title="Vercel FastAPI template",
-    description="A starter template for FastAPI backends in Vercel deployments",
-    version="0.1.0",
-    docs_url='/api',
-    openapi_url='/api/openapi.json',
-    redoc_url=None
-)
+app = FastAPI()
 
 
 @app.get('/api/hello')
