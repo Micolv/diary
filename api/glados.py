7356904376d3a045569cb6e98206bd5379affc30
thund1r <thuncoder@foxmail.com>
2022-05-12 12:15:00 +0800

    TEST

---
+++ api/glados.py
@@ -0,0 +1,33 @@
+import config
+import json
+import requests
+
+
+def checkin():
+    if config.get("glados_cookie"):
+        cookie = '_ga=GA1.2.360396571.1652294051; _gid=GA1.2.1042683980.1652294051; koa:sess=eyJ1c2VySWQiOjEzNDA4NSwiX2V4cGlyZSI6MTY3ODIxNDEwODMxMCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=eR4Pv07mpEFifxEjTvJctbH_dHI; _gat_gtag_UA_104464600_2=1'
+        url = "https://glados.rocks/api/user/checkin"
+        url2 = "https://glados.rocks/api/user/status"
+        origin = "https://glados.rocks"
+        referer = "https://glados.rocks/console/checkin"
+        useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+        payload = {
+            'token': 'glados.network'
+        }
+        checkin = requests.post(url, headers={'cookie': cookie, 'referer': referer, 'origin': origin,
+                                'user-agent': useragent, 'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload))
+        state = requests.get(url2, headers={
+            'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent})
+        if 'message' in checkin.text:
+            title = 'GLaDOS签到成功'
+            time = state.json()['data']['leftDays']
+            time = time.split('.')[0]
+            content = checkin.json()['message'] + '，有效期还剩' + time + '天'
+        else:
+            title = 'GLaDOS签到失败'
+            content = 'Cookie过期'
+        msg = title+","+content
+        push.push_msg(title, content)
+        return {"code": 200, "msg": msg}
+    else:
+        return {"code": 403, "msg": "请在环境变量中配置GLADOS_COOKIE"}
--- api/index.py
+++ api/index.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import requests
-import os
 import json
+import config
+import glados
+
 
 app = FastAPI()
 html = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>DailyCheckin</title>
+        <title>VercelCheckin</title>
     </head>
     <body>
-        <h1>DailyCheckin is running</h1>
+        <h1>VercelCheckin is running</h1>
+        <p>推送类型:<%PUSHTYPE%/></p>
+        <p>推送秘钥:<%PUSHTOKEN%/></p>
+        <p>GLADOS配置:<%GLADOS%/></p>
         <script>
         </script>
     </body>
@@ -20,51 +25,14 @@ html = """
 """
 
 
-def get_pic():
-    pic_url = "https://api.btstu.cn/sjbz/api.php?format=json&lx=fengjing"
-    r = requests.get(pic_url).json()
-    return r["imgurl"]
-
-
-def push_msg(title, content):
-    data = {
-        "secret": "vanmay",
-        "type": "textcard",
-        "title": title,
-        "content": content,
-    }
-    push_url = "https://wpush-thund1r.vercel.app/send"
-    body = json.dumps(data).encode(encoding='utf-8')
-    headers = {'Content-Type': 'application/json'}
-    requests.post(push_url, data=body, headers=headers)
-
-
-@app.get('/gla')
-async def gla():
-    cookie = '_ga=GA1.2.360396571.1652294051; _gid=GA1.2.1042683980.1652294051; koa:sess=eyJ1c2VySWQiOjEzNDA4NSwiX2V4cGlyZSI6MTY3ODIxNDEwODMxMCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=eR4Pv07mpEFifxEjTvJctbH_dHI; _gat_gtag_UA_104464600_2=1'
-    url = "https://glados.rocks/api/user/checkin"
-    url2 = "https://glados.rocks/api/user/status"
-    origin = "https://glados.rocks"
-    referer = "https://glados.rocks/console/checkin"
-    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-    payload = {
-        'token': 'glados.network'
-    }
-    checkin = requests.post(url, headers={'cookie': cookie, 'referer': referer, 'origin': origin,
-                            'user-agent': useragent, 'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload))
-    state = requests.get(url2, headers={
-                         'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent})
-    if 'message' in checkin.text:
-        title = 'GLaDOS签到成功'
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
-        content = checkin.json()['message'] + '，有效期还剩' + time + '天'
-    else:
-        title = 'GLaDOS签到失败'
-        content = 'Cookie过期'
-    push_msg(title, content)
+@app.get('/glados')
+async def glados():
+    return glados.checkin()
 
 
 @app.get('/')
 async def index():
-    return HTMLResponse(html)
+    push_type = config.get("push_type")
+    push_token = config.get("push_token")
+    glados_cookie = config.get("glados_cookie")
+    return HTMLResponse(html.replace('<%PUSHTYPE%/>', push_type).replace('<%PUSHTOKEN%/>', push_token).replace('<%GLADOS%/>', glados_cookie))
