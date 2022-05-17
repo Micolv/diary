38b6b78de92f21b3c5c3e5be3654b081c5048d75
thund1r <thuncoder@foxmail.com>
2022-05-18 02:07:48 +0800

    TEST

--- api/glados.py
+++ api/glados.py
@@ -1,9 +1,11 @@
 from . import config, push
 import json
 import requests
+import time
 
 
 def checkin():
+    old_time = time.time()
     if config.get("glados_cookie"):
         cookie = config.get("glados_cookie")
         url = "https://glados.rocks/api/user/checkin"
@@ -20,27 +22,30 @@ def checkin():
             state = requests.get(url2, headers={
                 'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent}).json()
             message = checkin['message']
-            if checkin['code'] == 0 or (checkin['code'] == 1 and 'list' in checkin):
+            if 'list' in checkin:
                 email = state['data']['email']
                 time = state['data']['leftDays'].split('.')[0]
                 title = "Glados签到成功，" + message
                 content = "账号：" + email + "\n\n剩余天数："+time + \
                     "天\n\n成功信息：" + message + "\n\n - - - "
                 push.push_msg(title, content)
-                return {"code": 200, "msg": title}
+                code = 200
             else:
                 title = "Glados签到失败，" + message
                 content = "错误信息：" + message + "，请尝试检查或更新参数。\n\n - - - "
                 push.push_msg(title, content)
-                return {"code": 401, "msg": title}
+                code = 401
         except Exception as errorMsg:
             print("Glados签到异常:", errorMsg)
             title = "Glados签到异常，" + repr(errorMsg)
             content = "异常信息：" + repr(errorMsg) + "，请检查控制台报错信息。\n\n - - - "
             push.push_msg(title, content)
-            return {"code": 500, "msg": title}
+            code = 500
     else:
-        title = "Glados配置缺失"
+        title = "Glados配置缺失，请在Vercel环境变量中配置GLADOS_COOKIE"
         content = "请在Vercel环境变量中配置GLADOS_COOKIE"
         push.push_msg(title, content)
-        return {"code": 401, "msg": "请在Vercel环境变量中配置GLADOS_COOKIE"}
+        code = 401
+    current_time = time.time()
+    print("运行时间为" + str(current_time - old_time) + "s")
+    return {"code": code, "msg": title}
